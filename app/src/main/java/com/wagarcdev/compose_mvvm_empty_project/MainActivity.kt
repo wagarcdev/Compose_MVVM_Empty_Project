9bf6f78eca8473d11268c32fb93afc486b8252a7
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2022-09-07 14:03:09 -0300

    name correction

--- app/src/main/java/com/wagarcdev/compose_mvvm_empty_project/MainActivity.kt
+++ app/src/main/java/com/wagarcdev/compose_mvvm_empty_project/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.wagarcdev.compose_mvvm_empty_project.navigation.AppNavigation
 import com.wagarcdev.compose_mvvm_empty_project.presentation.ui.theme.Compose_MVVM_Empty_ProjectTheme
@@ -19,13 +12,13 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         installSplashScreen().apply {
-            setKeepOnScreenCondition { viewModel.isLoading.value }
+            setKeepOnScreenCondition { mainViewModel.isLoading.value }
         }
 
         setContent {
